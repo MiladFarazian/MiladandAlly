68e594b19deeb85320c91e4775fcb8e83a687c04
Milad Farazian <mfarazia@ucsc.edu>
2020-06-11 20:32:16 -0700

    index edited

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 $(".carousel").carousel();
+
+$("body").scrollspy({ target: ".navbar", offset: 50 });
+
+$("#myNavbar a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
