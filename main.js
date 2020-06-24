fd7ea23300fbc3e90d119f4ec54f2b4b019ecdbb
Milad Farazian <mfarazia@ucsc.edu>
2020-06-23 20:38:56 -0700

    Added files

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(".carousel").carousel();
 
 $("body").scrollspy({ target: ".navbar", offset: 50 });
 
-$("#myNavbar a").on("click", function(event) {
+$("#myNavbar a").on("click", function (event) {
   if (this.hash !== "") {
     event.preventDefault();
 
@@ -10,10 +10,33 @@ $("#myNavbar a").on("click", function(event) {
 
     $("html, body").animate(
       {
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top,
       },
       400,
-      function() {}
+      function () {}
     );
   }
+});
+
+// Create the dropdown base
+$("<select />").appendTo("nav");
+
+// Create default option "Go to..."
+$("<option />", {
+  selected: "selected",
+  value: "",
+  text: "Go to...",
+}).appendTo("nav select");
+
+// Populate dropdown with menu items
+$("nav a").each(function () {
+  var el = $(this);
+  $("<option />", {
+    value: el.attr("href"),
+    text: el.text(),
+  }).appendTo("nav select");
+});
+
+$("nav select").change(function () {
+  window.location = $(this).find("option:selected").val();
 });
