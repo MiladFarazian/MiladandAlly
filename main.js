6215eba75940bd525959c672670332eacc682a74
Milad Farazian <mfarazia@ucsc.edu>
2020-06-24 22:51:47 -0700

    Added files

--- main.js
+++ main.js
@@ -17,3 +17,15 @@ $("#myNavbar a").on("click", function (event) {
     );
   }
 });
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
