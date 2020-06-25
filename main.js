b2d31b58288402614f85321e580848903cacec85
Milad Farazian <mfarazia@ucsc.edu>
2020-06-24 23:04:03 -0700

    Added files

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(".carousel").carousel();
 
 $("body").scrollspy({ target: ".navbar", offset: 50 });
 
-$("#myNavbar a").on("click", function (event) {
+/*$("#myNavbar a").on("click", function (event) {
   if (this.hash !== "") {
     event.preventDefault();
 
@@ -16,7 +16,7 @@ $("#myNavbar a").on("click", function (event) {
       function () {}
     );
   }
-});
+});*/
 
 window.onscroll = function () {
   scrollFunction();
