0475b1545574019dda88c6b59ddf5d7f243b6c75
Milad Farazian <mfarazia@ucsc.edu>
2020-06-24 23:13:59 -0700

    Added files

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.fontSize = "15px";
   } else {
-    document.getElementById("header").style.fontSize = "25px";
+    document.getElementById("header").style.fontSize = "20px";
   }
 }
