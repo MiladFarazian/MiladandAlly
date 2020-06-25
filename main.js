d883cfcdbfa304f57a5d0daaf626aeef444cc9f6
Milad Farazian <mfarazia@ucsc.edu>
2020-06-24 23:09:03 -0700

    Added files

--- main.js
+++ main.js
@@ -2,22 +2,6 @@ $(".carousel").carousel();
 
 $("body").scrollspy({ target: ".navbar", offset: 50 });
 
-/*$("#myNavbar a").on("click", function (event) {
-  if (this.hash !== "") {
-    event.preventDefault();
-
-    var hash = this.hash;
-
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top,
-      },
-      400,
-      function () {}
-    );
-  }
-});*/
-
 window.onscroll = function () {
   scrollFunction();
 };
@@ -26,6 +10,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.fontSize = "15px";
   } else {
-    document.getElementById("header").style.fontSize = "30px";
+    document.getElementById("header").style.fontSize = "25px";
   }
 }
